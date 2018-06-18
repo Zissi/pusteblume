eea178d2550e1255344d2e33162f3b9ce26a7fb5
Zissi <f.schlesinger@gmx.net>
2018-06-18 16:33:47 +0200

    Tidy up code

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-#from tilt_lights import light_on_tilt
-#from breath_led import breath_led
-#from display_tilt import display_tilt
-#from temp import read_sensors
-#from pusteblume import read_sensors
 import pyb
-
-# Display output on LCD
-#uart = pyb.UART('XA', 115200)
-#pyb.repl_uart(uart)
-
 import lcd160cr
-from time import sleep
 import machine
 
 lcd = lcd160cr.LCD160CR('X')
 sw = pyb.Switch()
-sw.callback(lambda:lcd.erase())
+sw.callback(lambda: lcd.erase())
 
 i2c = machine.I2C(sda=machine.Pin('Y10'), scl=machine.Pin('Y9'), freq=400000)
 i2c.scan()
@@ -23,12 +12,6 @@ i2c.scan()
 dt = 20
 b1 = bytearray(1)
 b2 = bytearray(2)
-
-last_temp = [0]
-last_hum = [0]
-last_image = 2
-
-
 
 
 def hdc1080_read(a=0):
@@ -51,72 +34,67 @@ def hdc_hum():
     return (t / 0x10000) * 100
 
 
-def read_sensors():
+def display_pusteblume(hum, average_hum_diff, tmp):
+    buf = bytearray(10000)
+    with open('/sd/pusteblume/s_p_{}.jpg'.format(last_image), 'rb') as f:
+        f.readinto(buf)
+        lcd.set_pos(0, 0)
+        lcd.jpeg(buf)
+        lcd.set_pos(5, 110)
+        lcd.set_font(1, scale=1)
+        lcd.set_text_color(lcd.rgb(188, 234, 231), lcd.rgb(64, 64, 128))
+        lcd.write('H ' + str(round(hum)))
+        lcd.set_pos(40, 130)
+        lcd.write(str(round(average_hum_diff, 2)))
+
+        lcd.set_pos(80, 110)
+        lcd.set_font(1, scale=1)
+        lcd.set_text_color(lcd.rgb(188, 234, 231), lcd.rgb(64, 64, 128))
+        lcd.write('T ' + str(round(tmp)))
+
+
+def calculate_average_diff(observations):
+    diffs = []
+    for i, obs in enumerate(observations):
+        if i < len(observations) - 1:
+            diffs.append((observations[i + 1] - obs) / (obs + 1))
+
+    return sum(diffs) / len(diffs)
+
+
+def read_sensors(last_temperatures, last_humidities, last_image):
     lcd.set_pen(lcd.rgb(255, 0, 0), lcd.rgb(64, 64, 128))
     temp = hdc_temp()
     hum = hdc_hum()
 
-    global last_temp
-    global last_hum
-    global last_image
-    global images
+    if abs(last_humidities[-1] - hum) > 0.5 or abs(last_temperatures[-1] - temp) > 0.5:
+        last_humidities.append(hum)
+        last_temperatures.append(temp)
 
-    if abs(last_hum[-1] - hum) > 1 or abs(last_temp[-1] - temp) > 1:
-        last_hum.append(hum)
-        last_temp.append(temp)
-        print("T: %.2f" % (temp))
-        print("H:  %.2f" % (hum))
-        if len(last_hum) > 10:
-            last_hum = last_hum[1:]
-            last_temp = last_temp[1:]
-    diffs = []
-    if len(last_hum) > 5:
-        for i, hum in enumerate(last_hum):
-            if i < len(last_hum) - 1:
-                diffs.append((last_hum[i + 1] - hum) / (hum + 1))
-        diff = sum(diffs) / len(diffs)
-        print(diff)
-        if diff < 0:
-            buf = bytearray(10000)
-            with open('/sd/pusteblume/s_p_{}.jpg'.format(last_image), 'rb') as f:
-                f.readinto(buf)
-                lcd.set_pos(0,0)
-                lcd.jpeg(buf)
-                lcd.set_pos(50,110)
-                lcd.set_font(1, scale=1)
-                lcd.set_text_color(lcd.rgb(188, 234, 231), lcd.rgb(64, 64, 128))
-                print(last_hum[-1])
-                print(str(round(last_hum[-1])))
-                lcd.write(str(round(last_hum[-1])))
-                lcd.set_pos(50,130)
-                lcd.write(str(round(diff, 2)))
-            if last_image == 28:
-                last_image = 28
-            else:
+        if len(last_humidities) > 5:
+            last_humidities = last_humidities[1:]
+            last_temperatures = last_temperatures[1:]
+
+        average_hum_diff = calculate_average_diff(last_humidities)
+
+        if average_hum_diff < 0:
+            display_pusteblume(hum, average_hum_diff, temp)
+
+            if last_image != 28:
                 last_image += 1
-        elif diff > 0.01 or hum > 90:
-            buf = bytearray(10000)
-            with open('/sd/pusteblume/s_p_{}.jpg'.format(last_image), 'rb') as f:
-                f.readinto(buf)
-                lcd.set_pos(0,0)
-                lcd.jpeg(buf)
-                lcd.set_pos(50,110)
-                lcd.set_font(1, scale=1)
-                lcd.set_text_color(lcd.rgb(188, 234, 231), lcd.rgb(64, 64, 128))
-                lcd.write(str(round(last_hum[-1])))
-                lcd.set_pos(50,130)
-                lcd.write(str(round(diff, 2)))
 
-                if last_image == 1:
-                    last_image = 1
-                else:
-                    last_image -= 1
+        elif average_hum_diff > 0.005 or hum > 90:
+            display_pusteblume(hum, average_hum_diff, temp)
+
+            if last_image != 1:
+                last_image -= 1
+
+    return last_temperatures, last_humidities, last_image
 
 
+last_temperatures = [0]
+last_humidities = [0]
+last_image = 2
 
 while True:
-    read_sensors()
-    #display_tilt()
-    #light_on_tilt()
-
-
+    last_temperatures, last_humidities, last_image = read_sensors(last_temperatures, last_humidities, last_image)
